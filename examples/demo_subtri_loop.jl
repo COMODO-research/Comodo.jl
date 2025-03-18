a0e42f6b70d4c7252927660b8b27f95a46017ad7
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2025-03-18 15:05:05 +0000

    Minor change to subtri demo

--- examples/demo_subtri_loop.jl
+++ examples/demo_subtri_loop.jl
@@ -5,7 +5,7 @@ using Comodo.GeometryBasics
 r = 1.0 #radius
 F,V = platonicsolid(4,r)
 
-# Fn,Vn = subTri(F,V,1; method=:loop)
+# Fn,Vn = subtri(F,V,n; method=:Loop) 
 
 ################# 
 
@@ -14,21 +14,17 @@ fig = Figure(size=(800,800))
 stepRange = 0:1:5
 hSlider = Slider(fig[2, 1], range = stepRange, startvalue = 0,linewidth=30)
 
-titleString = lift(hSlider.value) do stepIndex
-    "n = " * string(stepIndex) * " refinement steps"
+titleString = lift(hSlider.value) do n
+    "n = " * string(n) * " refinement steps"
 end
 
-Mn = lift(hSlider.value) do stepIndex
-    Fn,Vn = subtri(F,V,stepIndex; method=:Loop) 
+Mn = lift(hSlider.value) do n
+    Fn,Vn = subtri(F,V,n; method=:Loop) 
     return GeometryBasics.Mesh(Vn,Fn)
 end
-
 ax = Axis3(fig[1, 1], aspect = :data, xlabel = "X", ylabel = "Y", zlabel = "Z", title = titleString)
-slidercontrol(hSlider,ax)
 
-hp1=wireframe!(ax,GeometryBasics.Mesh(V,F),linewidth=3,color=:red, overdraw=false)
-hp2=poly!(ax,Mn,strokewidth=1,color=:white, shading = FastShading)
-
-# hp2=poly!(scene,M,strokewidth=1,color=:white, shading = FastShading)
+hp1 = wireframe!(ax,GeometryBasics.Mesh(V,F),linewidth=3,color=:red, overdraw=false)
+hp2 = poly!(ax,Mn,strokewidth=1,color=:white, shading = FastShading)
 
 fig
