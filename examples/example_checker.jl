1c098cf71ca8631181ee5bc958502301cc8c396b
jbytecode <mhsatman@gmail.com>
2025-03-27 11:58:01 +0300

    introduce multitasking checking capabilities in example checker

--- examples/example_checker.jl
+++ examples/example_checker.jl
@@ -1,5 +1,7 @@
 module ExampleChecker
 
+const NUMTASKS = 10
+
 function load_and_run!(filename::String, problems::Vector{String})::Bool
     #cmdline = "using Pkg; Pkg.activate(\"./..\"); include(\"$filename\")"
 		cmdline = "using Pkg; Pkg.activate(\".\"); Pkg.instantiate(); include(\"$filename\")"
@@ -22,9 +24,16 @@ function main()
     juliafiles = filter(x -> occursin(r"demo.*\.jl", x), dircontent)
 
     problems = Vector{String}(undef, 0)
+    tasks = Task[]
     for file in juliafiles
+        if length(tasks) >= NUMTASKS
+            wait.(tasks)
+            empty!(tasks)
+        end
         println("Checking file: $file")
-        load_and_run!(file, problems)
+        t = Task(() -> load_and_run!(file, problems))
+        push!(tasks, t)
+        schedule(t)
     end
 
     if !isempty(problems)
