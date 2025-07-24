14f1b1022ab0937ac094844f4ca81dacf2d8a283
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2025-07-24 09:35:49 +0100

    Attempt to increase documentation site size threshold

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,8 @@ makedocs(
          format = Documenter.HTML(
                                   prettyurls = get(ENV, "CI", nothing) == "true",
                                   collapselevel = 2,
+                		  size_threshold_warn=1500 * 2^10, 
+        			  size_threshold=2000 * 2^10, 
                                   # assets = ["assets/favicon.ico", "assets/extra_styles.css"],
                                  ),
          sitename="A Julia package for computational (bio)mechanics and computational design",
