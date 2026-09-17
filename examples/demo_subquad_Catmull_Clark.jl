7ff2b87eef7dd68ee4eef76e0e20890b9b4ce402
Kevin-Mattheus-Moerman <kevin.moerman@gmail.com>
2026-09-17 09:49:50 +0100

    Minor change to subtri loop and subquad Catmull-Clarke demos

--- examples/demo_subquad_Catmull_Clark.jl
+++ examples/demo_subquad_Catmull_Clark.jl
@@ -5,37 +5,64 @@ using Comodo.Statistics
 using Comodo.LinearAlgebra
 
 ## Define example input
-r = 1.0 #radius
-F,V = platonicsolid(2,r) # Get an example quadrilateral mesh (for a cube in this case)
+F1, V1 = platonicsolid(2, 1.0) # Get an example quadrilateral mesh (for a cube in this case)
 
-## Refine mesh using `subquad` and the Catmull-Clark method
-Fn1,Vn1 = subquad(F,V,1; method = :Catmull_Clark) # Refined once 
-Fn2,Vn2 = subquad(F,V,2; method = :Catmull_Clark) # Refined twice
-Fn3,Vn3 = subquad(F,V,3; method = :Catmull_Clark) # Refined 3 times
+fileName_mesh = joinpath(comododir(), "assets", "obj", "spot_quadrangulated.obj")
 
-Rn3 = [norm(v) for v in Vn3] 
-rs3 = mean(Rn3)
-d3 = Rn3.-rs3
+function import_obj(loadName)
+    file_IO = open(loadName)
+    F = Vector{NgonFace}()
+    V = Vector{Point{3,Float64}}()
+    for line in eachline(file_IO)        
+        splitLine = strip.(split(line, " "))
+        if splitLine[1] == "f"            
+            f = Vector{Int}()
+            for s in splitLine[2:end] 
+                s_split = split(s, "/")
+                push!(f, parse(Int64, s_split[1]))
+            end
+            push!(F, NgonFace{length(f), Int}(f))
+        elseif splitLine[1] == "v"
+            push!(V, Point{3,Float64}(parse.(Float64, splitLine[2:end])))
+        end
+    end
+    close(file_IO)
+    return F, V
+end
 
-## Visualization
+F2, V2 = import_obj(fileName_mesh)
+F2 = [f for f in F2]
+V2 = [Point{3, Float64}(p[3], p[1], p[2]) for p in V2]
+
+n = 1
+
+F1n, V1n = subquad(F1, V1, n; method=:Catmull_Clark)
+F2n, V2n = subquad(F2, V2, n; method=:Catmull_Clark)
+
+# Visualisation
 GLMakie.closeall()
 
-fig = Figure(size=(800,800))
+fig = Figure(size=(1200, 800))
+ax1 = AxisGeom(fig[1, 1], title="n = " * string(n) * " refinement steps")
+hp1 = meshplot!(ax1, F1n, V1n; strokewidth=0.5)
+hp2 = edgeplot!(ax1, F1, V1, linewidth=3, color=:red, depth_shift=-0.01f0)
 
-ax1 = AxisGeom(fig[1, 1], title = "Refined n=1")
-edgeplot!(ax1, F, V, linewidth=8,color=:red)
-meshplot!(ax1, Fn1, Vn1, strokewidth=3)
 
-ax2 = AxisGeom(fig[1, 2], title = "Refined n=2")
-edgeplot!(ax2, F, V, linewidth=8,color=:red)
-meshplot!(ax2, Fn2, Vn2, strokewidth=3)
+ax2 = AxisGeom(fig[1, 2], title="n = " * string(n) * " refinement steps")
+hp3 = meshplot!(ax2, F2n, V2n; strokewidth=0.5)
+hp4 = edgeplot!(ax2, F2, V2, linewidth=1, color=:red, depth_shift=-0.01f0)
 
-ax3 = AxisGeom(fig[2, 1], title = "Refined n=3")
-hp1 = edgeplot!(ax3, F, V, linewidth=8,color=:red)
-hp2 = meshplot!(ax3, Fn3, Vn3, strokewidth=3)
-Legend(fig[2, 1][1,2],[hp1,hp2],["Initial","Refined"])
+stepRange = 0:1:3
+hSlider = Slider(fig[2, :], range=stepRange, startvalue=1, linewidth=30)
 
-ax4 = AxisGeom(fig[2, 2], title = "Refined n=3, distance to sphere")
-hp3 = meshplot!(ax4, Fn3, Vn3, strokewidth=0.5, color=d3, colormap=:Spectral)
-Colorbar(fig[2, 2][1, 2],hp3)
+on(hSlider.value) do n
+    F1n, V1n = subquad(F1, V1, n; method=:Catmull_Clark)
+    F2n, V2n = subquad(F2, V2, n; method=:Catmull_Clark)
+
+    ax1.title = "n = " * string(n) * " refinement steps"
+    ax2.title = "n = " * string(n) * " refinement steps"
+    hp1[1] = GeometryBasics.Mesh(V1n, F1n)
+    hp3[1] = GeometryBasics.Mesh(V2n, F2n)
+end
+
 fig
--- examples/demo_subtri_loop.jl
+++ examples/demo_subtri_loop.jl
@@ -1,28 +1,46 @@
 using Comodo
 using Comodo.GLMakie
 using Comodo.GeometryBasics
+using FileIO
 
-r = 1.0 #radius
-F,V = platonicsolid(4,r)
+F1, V1 = platonicsolid(4, 1.0)
+
+fileName_mesh = joinpath(comododir(), "assets", "obj", "spot_control_mesh.obj")
+M = load(fileName_mesh)
+# Obtain mesh faces and vertices
+F2 = tofaces(faces(M))
+V2 = [Point{3, Float64}(p[3], p[1], p[2]) for p in coordinates(M)]
+F2, V2, _, _ = mergevertices(F2, V2)
 
 n = 1
-Fn,Vn = subtri(F,V,n; method=:Loop) 
+
+F1n, V1n = subtri(F1, V1, n; method=:Loop)
+F2n, V2n = subtri(F2, V2, n; method=:Loop)
 
 # Visualisation
 GLMakie.closeall()
 
-fig = Figure(size=(800,800))
-ax = AxisGeom(fig[1, 1], title = "n = " * string(n) * " refinement steps")
-hp1 = edgeplot!(ax, F, V, linewidth=3, color=:red)
-hp2 = meshplot!(ax, Fn, Vn)
+fig = Figure(size=(1200, 800))
+ax1 = AxisGeom(fig[1, 1], title="n = " * string(n) * " refinement steps")
+hp1 = meshplot!(ax1, F1n, V1n; strokewidth=0.5)
+hp2 = edgeplot!(ax1, F1, V1, linewidth=3, color=:red, depth_shift=-0.01f0)
 
-stepRange = 0:1:5
-hSlider = Slider(fig[2, 1], range = stepRange, startvalue = 1,linewidth=30)
 
-on(hSlider.value) do n    
-    Fn,Vn = subtri(F,V,n; method=:Loop)     
-    ax.title = "n = " * string(n) * " refinement steps"
-    hp2[1] = GeometryBasics.Mesh(Vn, Fn)
+ax2 = AxisGeom(fig[1, 2], title="n = " * string(n) * " refinement steps")
+hp3 = meshplot!(ax2, F2n, V2n; strokewidth=0.25)
+hp4 = edgeplot!(ax2, F2, V2, linewidth=1, color=:red, depth_shift=-0.01f0)
+
+stepRange = 0:1:3
+hSlider = Slider(fig[2, :], range=stepRange, startvalue=1, linewidth=30)
+
+on(hSlider.value) do n
+    F1n, V1n = subtri(F1, V1, n; method=:Loop)
+    F2n, V2n = subtri(F2, V2, n; method=:Loop)
+    
+    ax1.title = "n = " * string(n) * " refinement steps"
+    ax2.title = "n = " * string(n) * " refinement steps"
+    hp1[1] = GeometryBasics.Mesh(V1n, F1n)
+    hp3[1] = GeometryBasics.Mesh(V2n, F2n)
 end
 
 fig
